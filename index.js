58a7ed3e8235aea18e4eb184fccbb64569548ed6
Ryan O'Connor <rocsteady888@gmail.com>
2019-05-30 22:44:12 -0400

    added routes

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Initialize Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000
 
