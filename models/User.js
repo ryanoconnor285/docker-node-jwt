240b44ed1cd351ba5b79e08402c6ab1dba8aee8d
Ryan O'Connor <rocsteady888@gmail.com>
2019-05-30 22:44:49 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('user', UserSchema)
